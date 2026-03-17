97845eebca3b2655f27e162df95d640576c3cee8
drumlast <drumlastic@gmail.com>
2026-03-17 13:51:40 +0300

    provider yandex moved to main.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,1 @@
-provider "yandex" {
-  cloud_id               = var.cloud_id
-  folder_id              = var.folder_id
-  service_account_key_file = var.service_account_key_file
-}
+# provider "yandex" is defined in main.tf
