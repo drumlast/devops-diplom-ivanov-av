7ee69709f5e2d416be7b0b303dce5202d35b0d98
drumlast <drumlastic@gmail.com>
2026-03-17 13:10:45 +0300

    changed main in bootstrap

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -38,18 +38,3 @@ resource "yandex_storage_bucket" "tf_state" {
   access_key = yandex_iam_service_account_static_access_key.sa_static_key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa_static_key.secret_key
 }
-
-output "terraform_sa_key_json" {
-  value     = yandex_iam_service_account_key.terraform_key.private_key
-  sensitive = true
-}
-
-output "storage_access_key" {
-  value     = yandex_iam_service_account_static_access_key.sa_static_key.access_key
-  sensitive = true
-}
-
-output "storage_secret_key" {
-  value     = yandex_iam_service_account_static_access_key.sa_static_key.secret_key
-  sensitive = true
-}
