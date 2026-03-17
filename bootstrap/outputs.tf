c1fdff3f21c340a76791c8612e35b0a31b348cd8
drumlast <drumlastic@gmail.com>
2026-03-17 14:15:28 +0300

    fix output in bootstrap to save key in json format

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -9,6 +9,13 @@ output "storage_secret_key" {
 }
 
 output "terraform_sa_key_json" {
-  value     = yandex_iam_service_account_key.terraform_key.private_key
+  value = jsonencode({
+    id                 = yandex_iam_service_account_key.terraform_key.id
+    service_account_id = yandex_iam_service_account.terraform.id
+    created_at         = yandex_iam_service_account_key.terraform_key.created_at
+    key_algorithm      = yandex_iam_service_account_key.terraform_key.key_algorithm
+    public_key         = yandex_iam_service_account_key.terraform_key.public_key
+    private_key        = yandex_iam_service_account_key.terraform_key.private_key
+  })
   sensitive = true
 }
